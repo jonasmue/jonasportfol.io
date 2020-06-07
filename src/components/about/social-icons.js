560eec8e8ac3c0ffb0f3dac201d0dd1c4f75b9b5
Jonas Müller <jonas.mueller@compitencies.com>
2020-06-07 02:29:49 +0200

    Change margin of social icons

--- src/components/about/social-icons.js
+++ src/components/about/social-icons.js
@@ -2,16 +2,16 @@ const SocialIcons = () => {
 	return (
 		<div className={"header mb-4 text-center text-xl"}>
 			<a href={"https://linkedin.com/in/jomue"} target={"_blank"}>
-				<i className={"fab fa-linkedin-in"}/>
+				<i className={"mr-4 fab fa-linkedin-in"}/>
 			</a>
 			<a href={"https://github.com/jonasmue"} target={"_blank"}>
-				<i className={"ml-8 fab fa-github"}/>
+				<i className={"ml-4 mr-4 fab fa-github"}/>
 			</a>
 			<a href={"/"}>
-				<span className={"ml-8 font-extrabold"}>CV</span>
+				<span className={"ml-4 mr-4 font-extrabold"}>CV</span>
 			</a>
 			<a href={"mailto:hej@jonasportfol.io"}>
-				<i className={"ml-8 fa fa-envelope"}/>
+				<i className={"ml-4 fa fa-envelope"}/>
 			</a>
 		</div>
 	)
