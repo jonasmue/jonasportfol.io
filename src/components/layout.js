cb136b866632488a920cd66bb793a66fd539ec86
Jonas Müller <jonas.mueller@compitencies.com>
2020-06-10 13:29:49 +0200

    Change og image

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,9 +16,7 @@ export default function Layout({children}) {
 				/>
 				<meta
 					property="og:image"
-					content={`https://og-image.now.sh/${encodeURI(
-						siteTitle
-					)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+					content="https://og-image.now.sh/**Jonas%20M%C3%BCller**%20%7C%20Portfolio.png?theme=light&md=1&fontSize=125px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg&widths=auto&heights=auto"
 				/>
 				<meta name="og:title" content={siteTitle}/>
 				<meta name="twitter:card" content="summary_large_image"/>
