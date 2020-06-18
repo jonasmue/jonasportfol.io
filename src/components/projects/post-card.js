160523bb70fbfd8d196707831ef5303542882395
Jonas Müller <jonas.mueller@compitencies.com>
2020-06-18 02:53:13 +0200

    Add satisfaction observer

--- src/components/projects/post-card.js
+++ src/components/projects/post-card.js
@@ -7,31 +7,61 @@ const PostCard = (props) => {
 	return (
 		<>
 			<div className={"w-full p-3 md:p-6" + pinnedClass}>
-				<Link href="/posts/[id]" as={`/posts/${props.data.id}`}>
-					<div className={"card rounded-lg shadow-2xl p-3"}>
-						<div className={"card-background"}>
-							<div className={"card-img"}
-								 style={{backgroundImage: "url(/images/posts/" + props.data.image + ")"}}/>
-							<div className={"card-gradient " + props.data.category.trim().replace(/\s+/g, '-').toLowerCase()}/>
-						</div>
-						<div className={"card-content"}>
-							<h3 className={"text-xl sm:text-2xl"}>{props.data.title}</h3>
-							<br/>
-							<p className={"card-description pb-16 text-sm sm:text-base mt-0"}>
-								{props.data.teaser || ""}
-							</p>
-							<div className={"card-bottom w-full"}>
-								<p className={"card-category mb-1"}>{props.data.category}</p>
-								<div className={"w-1/4"}>
-									<hr/>
+				{/* Duplicate code sucks, but is due to bug in Next.js */}
+				{!props.data.link ?
+					<Link href="/posts/[id]" as={`/posts/${props.data.id}`}>
+						<div className={"card rounded-lg shadow-2xl p-3"}>
+							<div className={"card-background"}>
+								<div className={"card-img"}
+									 style={{backgroundImage: "url(/images/posts/" + props.data.image + ")"}}/>
+								<div
+									className={"card-gradient " + props.data.category.trim().replace(/\s+/g, '-').toLowerCase()}/>
+							</div>
+							<div className={"card-content"}>
+								<h3 className={"text-xl sm:text-2xl"}>{props.data.title}</h3>
+								<br/>
+								<p className={"card-description pb-16 text-sm sm:text-base mt-0"}>
+									{props.data.teaser || ""}
+								</p>
+								<div className={"card-bottom w-full"}>
+									<p className={"card-category mb-1"}>{props.data.category}</p>
+									<div className={"w-1/4"}>
+										<hr/>
+									</div>
+									<small>
+										<Date className={"card-date"} dateString={props.data.date}/>
+									</small>
 								</div>
-								<small>
-									<Date className={"card-date"} dateString={props.data.date}/>
-								</small>
 							</div>
 						</div>
-					</div>
-				</Link>
+					</Link> :
+					<a target={"_blank"} href={props.data.link}>
+						<div className={"card rounded-lg shadow-2xl p-3"}>
+							<div className={"card-background"}>
+								<div className={"card-img"}
+									 style={{backgroundImage: "url(/images/posts/" + props.data.image + ")"}}/>
+								<div
+									className={"card-gradient " + props.data.category.trim().replace(/\s+/g, '-').toLowerCase()}/>
+							</div>
+							<div className={"card-content"}>
+								<h3 className={"text-xl sm:text-2xl"}>{props.data.title}</h3>
+								<br/>
+								<p className={"card-description pb-16 text-sm sm:text-base mt-0"}>
+									{props.data.teaser || ""}
+								</p>
+								<div className={"card-bottom w-full"}>
+									<p className={"card-category mb-1"}>{props.data.category}</p>
+									<div className={"w-1/4"}>
+										<hr/>
+									</div>
+									<small>
+										<Date className={"card-date"} dateString={props.data.date}/>
+									</small>
+								</div>
+							</div>
+						</div>
+					</a>
+				}
 			</div>
 		</>
 	)
---
+++ src/posts/satisfaction-observer.md
@@ -0,0 +1,8 @@
+---
+title: 'Satisfaction Observer'
+date: '2020-06-18'
+category: "Natural Language Processing"
+image: "satisfaction-observer.png"
+teaser: "Sentiment Analysis Project: how positive (or negative) are tweets that people write about their head of government?"
+link: "https://satisfaction.observer"
+---
