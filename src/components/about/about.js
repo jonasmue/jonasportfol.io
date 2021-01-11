ab6ff37b1bdae6b96ca35eae673807ac1d07295a
Jonas Müller <jonas.mueller@compitencies.com>
2021-01-11 19:08:42 +0100

    Update about me

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -18,13 +18,15 @@ const AboutMe = ({salutation}) => {
 			</div>
 			<div className={"text-center mb-4 ml-auto mr-auto w-1/4 rounded-lg border-b-2 border-gray"}/>
 			<SocialIcons/>
-			<p className={"md:text-xl"}>I am a recent graduate of the Master's <a href={"https://hci.uni-wuerzburg.de"}
+			<p className={"md:text-xl"}>I am a consultant at <a href={"https://netlight.com"}>Netlight</a>.
+
+				Before that I graduated in the the Master's <a href={"https://hci.uni-wuerzburg.de"}
 																				  target={"_blank"}>Human-Computer
 				Interaction</a> program at the University of Würzburg. <br/>
 				Apart from "classic HCI topics"
 				I am passionate about researching into and applying Natural Language Processing to create enhanced
 				user experiences. This site is dedicated
-				to showcase some of my recent university and private projects.
+				to showcase some of my recent side and university projects.
 			</p>
 		</section>
 	)
